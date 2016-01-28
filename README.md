4bea562f1d51515c7cef1fd19ff269b592858dde
deepsweet <kir@soulshine.in>
2016-01-28 17:13:39 +0700

    :hatching_chick:

---
+++ README.md
@@ -0,0 +1,53 @@
+[![npm](https://img.shields.io/npm/v/rebem-enzyme.svg?style=flat-square)](https://www.npmjs.com/package/rebem-enzyme)
+[![travis](http://img.shields.io/travis/rebem/enzyme.svg?style=flat-square)](https://travis-ci.org/rebem/enzyme)
+[![coverage](https://img.shields.io/codecov/c/github/rebem/enzyme.svg?style=flat-square)](https://codecov.io/github/rebem/enzyme)
+[![deps](https://img.shields.io/gemnasium/rebem/enzyme.svg?style=flat-square)](https://gemnasium.com/rebem/enzyme)
+
+[reBEM](https://github.com/rebem/rebem) addons for [Enzyme](http://airbnb.io/enzyme/).
+
+## Install
+
+```
+npm i -D rebem-enzyme
+```
+
+## Overview
+
+In addition to usual Enzyme methods there are few new which lets you search for components by [BEM PropTypes](https://github.com/rebem/rebem#bem-proptypes):
+
+* `block`
+* `elem`
+* `mods`
+* `mix`
+* `tag`
+
+### findBEM(bem)
+
+```js
+import { BEM } from 'rebem';
+import { mount, shallow } from 'rebem-enzyme';
+
+// Full DOM Rendering
+const reactWrapper = mount(
+    BEM({ block: 'block' },
+        BEM({ block: 'block', elem: 'elem' })
+    )
+);
+
+console.log(
+    reactWrapper.findBEM({ block: 'block', elem: 'elem' }).length
+);
+// 1
+
+// Shallow Rendering
+const shallowWrapper = shallow(
+    BEM({ block: 'block' },
+        BEM({ block: 'block', elem: 'elem' })
+    )
+);
+
+console.log(
+    reactWrapper.findBEM({ block: 'block', elem: 'elem' }).length
+);
+// 1
+```
---
+++ test/lib/index.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import assert from 'assert';
+
+import { mount, shallow } from '../../lib/';
+
+const props = {
+    block: 'block',
+    elem: 'elem',
+    mods: {
+        mod: 'val'
+    },
+    mix: [
+        {
+            block: 'block2',
+            elem: 'elem2'
+        }
+    ],
+    tag: 'span'
+};
+const bemjson = { ...props };
+
+class TestSingle extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement('span', props)
+        );
+    }
+}
+
+class TestMultiple extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement('span', props),
+            React.createElement('span', props)
+        );
+    }
+}
+
+describe('reBEM Enzyme', function() {
+    describe('mount', function() {
+        describe('findBEM', function() {
+            it('is function', function() {
+                const wrapper = mount(
+                    React.createElement('div')
+                );
+
+                assert.strictEqual(
+                    typeof wrapper.findBEM,
+                    'function'
+                );
+            });
+
+            it('simple', function() {
+                const wrapper = mount(
+                    React.createElement(TestSingle)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM(bemjson).length,
+                    1
+                );
+            });
+
+            it('multiple', function() {
+                const wrapper = mount(
+                    React.createElement(TestMultiple)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM(bemjson).length,
+                    2
+                );
+            });
+
+            it('not found', function() {
+                const wrapper = mount(
+                    React.createElement(TestMultiple)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM({ block: 'beep' }).length,
+                    0
+                );
+            });
+        });
+    });
+
+    describe('shallow', function() {
+        describe('findBEM', function() {
+            it('is function', function() {
+                const wrapper = shallow(
+                    React.createElement('div')
+                );
+
+                assert.strictEqual(
+                    typeof wrapper.findBEM,
+                    'function'
+                );
+            });
+
+            it('simple', function() {
+                const wrapper = shallow(
+                    React.createElement(TestSingle)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM(bemjson).length,
+                    1
+                );
+            });
+
+            it('multiple', function() {
+                const wrapper = shallow(
+                    React.createElement(TestMultiple)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM(bemjson).length,
+                    2
+                );
+            });
+
+            it('not found', function() {
+                const wrapper = shallow(
+                    React.createElement(TestMultiple)
+                );
+
+                assert.strictEqual(
+                    wrapper.findBEM({ block: 'beep' }).length,
+                    0
+                );
+            });
+        });
+    });
+});
